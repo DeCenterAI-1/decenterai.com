719d74952f24e4d206621fbcd9dbd78aa7e728d0
Kosawat Sukchaya <kosawat@gmail.com>
2025-11-05 11:03:27 +0100

    Removed workflow and updated team

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       <Demo />
       <Features />
       <Token />
-      <WorkFlow />
+      {/* <WorkFlow /> */}
       <Team />
       <Mail />
       {/* <Action /> */}
